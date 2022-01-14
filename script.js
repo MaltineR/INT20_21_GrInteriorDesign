7c01579fbf12eeb99cc1e09fb70354dbf8227ca3
HP <HP@DESKTOP-5EUSGUG>
2022-01-14 21:33:18 +0100

    changes

--- script.js
+++ script.js
@@ -43,6 +43,37 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 }
+//countdown counter 
+var countDownDate = new Date("Jan 31, 2022 16:37:52").getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + "Days "
+    document.getElementById("hours").innerHTML = hours + "Hours " 
+    document.getElementById("mins").innerHTML = minutes + "Mins " 
+    document.getElementById("secs").innerHTML = seconds + "Secs " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TIME UP!!";
+    }
+    }, 1000);
 //startworker 
 let w;
 
@@ -60,6 +91,13 @@ function stopWorker() {
   w = undefined;
 }
 //what is palette
+$(document).ready(function(){
+  $("button1").click(function(){
+    var div = $("div2");  
+    div.animate({left: '800px'}, "slow");
+    div.animate({fontSize: '2em'}, "slow");
+  });
+});
 
  //colors 
 
